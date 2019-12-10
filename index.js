b616a17a8c5e07ffcc7d402f5d2be276385d4c66
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 18:05:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let newArray = [];
+function writeCards(names, events) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+
+return newArray;
+}
+
+let newNumbers = [];
+function countDown(number){
+while (number >= 0) {
+  console.log(number--);
+}
+}
